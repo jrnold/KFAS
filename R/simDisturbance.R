678dd0bb454897a21867c4eec9abad267757760a
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-05-27 15:27:30 -0400

    change object$r to object$k

--- R/simDisturbance.R
+++ R/simDisturbance.R
@@ -16,11 +16,11 @@ simDisturbance <- function(object, nsim = 1, antithetics = FALSE) {
 
     if (identical(object$H_type, "Augmented")) {
         
-        etaplus <- array(0, c(object$r, object$n, nsim))
+        etaplus <- array(0, c(object$k, object$n, nsim))
         aplus1 <- array(0, dim = c(object$m, nsim))
         
-        x2 <- array(apply(object$Q, 3, diag) > object$tol0, c(object$r, (object$n - 1) * tv[5] + 1))
-        x2 <- array(x2, c(object$r, object$n, nsim))
+        x2 <- array(apply(object$Q, 3, diag) > object$tol0, c(object$k, (object$n - 1) * tv[5] + 1))
+        x2 <- array(x2, c(object$k, object$n, nsim))
         dfeta <- sum(x2)/nsim
         
         nde <- which(diag(object$P1) > object$tol0)
@@ -46,14 +46,14 @@ simDisturbance <- function(object, nsim = 1, antithetics = FALSE) {
             sims.out <- .Fortran("distsima", PACKAGE = "KFAS", NAOK = TRUE, ymiss = ymiss, tv=as.integer(tv), 
                     yt = array(object$y,dim=c(object$n,object$p)), Zt = object$Z, Tt = object$T, Rt = object$R, 
                     Qt = object$Q, a1 = object$a1, P1 = object$P1, object$P1inf, as.integer(nnd), as.integer(nsim),etaplus = etaplus, 
-                    aplus1 = aplus1, p=object$p, n=object$n, m=object$m, r=object$r, info = as.integer(0), tolF = object$tolF, 
-                    rankp = as.integer(sum(object$P1inf)), c = c2, etasim = array(0, c(object$r, object$n, nsim2)), object$tol0)
+                    aplus1 = aplus1, p=object$p, n=object$n, m=object$m, r=object$k, info = as.integer(0), tolF = object$tolF, 
+                    rankp = as.integer(sum(object$P1inf)), c = c2, etasim = array(0, c(object$k, object$n, nsim2)), object$tol0)
         } else {
             sims.out <- .Fortran("distsimnata", PACKAGE = "KFAS", NAOK = TRUE, ymiss = ymiss, tv=as.integer(tv), 
                     yt = array(object$y,dim=c(object$n,object$p)), Zt = object$Z, Tt = object$T, Rt = object$R, Qt = object$Q, 
                     a1 = object$a1, P1 = object$P1, object$P1inf, as.integer(nnd), as.integer(nsim), etaplus = etaplus, 
-                    aplus1 = aplus1, p=object$p, n=object$n, m=object$m, r=object$r, info = as.integer(0), tolF = object$tolF, 
-                    rankp = as.integer(sum(object$P1inf)), etasim = array(0, c(object$r, object$n, nsim2)), object$tol0)
+                    aplus1 = aplus1, p=object$p, n=object$n, m=object$m, r=object$k, info = as.integer(0), tolF = object$tolF, 
+                    rankp = as.integer(sum(object$P1inf)), etasim = array(0, c(object$k, object$n, nsim2)), object$tol0)
         }
         sims <- list(eta = sims.out$etasim[, , 1:nsim, drop = FALSE])
     } else {
@@ -61,15 +61,15 @@ simDisturbance <- function(object, nsim = 1, antithetics = FALSE) {
         
         
         epsplus <- array(0, c(object$p, object$n, nsim))
-        etaplus <- array(0, c(object$r, object$n, nsim))
+        etaplus <- array(0, c(object$k, object$n, nsim))
         aplus1 <- array(0, dim = c(object$m, nsim))
         
         x <- (array(apply(object$H, 3, diag) > object$tol0, c(object$p, object$n)))
         x <- array(x, c(object$p, object$n, nsim))
         dfeps <- sum(x)/nsim
         
-        x2 <- array(apply(object$Q, 3, diag) > object$tol0, c(object$r, (object$n - 1) * tv[5] + 1))
-        x2 <- array(x2, c(object$r, object$n, nsim))
+        x2 <- array(apply(object$Q, 3, diag) > object$tol0, c(object$k, (object$n - 1) * tv[5] + 1))
+        x2 <- array(x2, c(object$k, object$n, nsim))
         dfeta <- sum(x2)/nsim
         
         nde <- which(diag(object$P1) > object$tol0)
@@ -98,15 +98,15 @@ simDisturbance <- function(object, nsim = 1, antithetics = FALSE) {
                     yt = array(object$y,dim=c(object$n,object$p)), Zt = object$Z, H = object$H, Tt = object$T, 
                     Rt = object$R, Qt = object$Q, a1 = object$a1, P1 = object$P1, object$P1inf, as.integer(nnd), 
                     as.integer(nsim), epsplus = epsplus, etaplus = etaplus, aplus1 = aplus1, object$p, object$n, object$m, 
-                    object$r, info = as.integer(0), tolF = object$tolF, rankp = as.integer(sum(object$P1inf)), 
-                    c = c2, epssim = array(0, c(object$p, object$n, nsim2)), etasim = array(0, c(object$r, object$n, nsim2)), object$tol0)
+                    object$k, info = as.integer(0), tolF = object$tolF, rankp = as.integer(sum(object$P1inf)), 
+                    c = c2, epssim = array(0, c(object$p, object$n, nsim2)), etasim = array(0, c(object$k, object$n, nsim2)), object$tol0)
         } else {
             sims.out <- .Fortran("distsimnatd", PACKAGE = "KFAS", NAOK = TRUE, ymiss = ymiss, tv=as.integer(tv), 
                     yt = array(object$y,dim=c(object$n,object$p)), Zt = object$Z, H = object$H, Tt = object$T, Rt = object$R, 
                     Qt = object$Q, a1 = object$a1, P1 = object$P1, object$P1inf, as.integer(nnd), as.integer(nsim), 
-                    epsplus = epsplus, etaplus = etaplus, aplus1 = aplus1, p=object$p, n=object$n, m=object$m, r=object$r, 
+                    epsplus = epsplus, etaplus = etaplus, aplus1 = aplus1, p=object$p, n=object$n, m=object$m, r=object$k, 
                     info = as.integer(0), tolF = object$tolF, rankp = as.integer(sum(object$P1inf)), 
-                    epssim = array(0, c(object$p, object$n, nsim2)), etasim = array(0, c(object$r, object$n, nsim2)), object$tol0)
+                    epssim = array(0, c(object$p, object$n, nsim2)), etasim = array(0, c(object$k, object$n, nsim2)), object$tol0)
             
         }
         sims <- list(eps = sims.out$epssim, eta = sims.out$etasim)
